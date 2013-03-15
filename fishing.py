3ec945bb5a24ac158e030463eb7f820dc5b1eb11
Bart Massey <bart@cs.pdx.edu>
2013-03-15 09:39:54 -0700

    finished support for high score user recording

--- fishing.py
+++ fishing.py
@@ -10,11 +10,23 @@
 
 import random
 
+score_file_name = "scores.txt"
+
 catches = [
     ("minnow", 40, 2),
     ("haddock", 10, 10),
     ("lobster", 5, 20),
     ("golden boot", 1, 100) ]
+
+# Current fisher.
+fisher = None
+# Current fisher's current score.
+score = 0
+
+# All-time high score fisher.
+high_score_fisher = None
+# All-time high score.
+high_score = 0
 
 def cast():
     selection = random.randrange(100)
@@ -26,15 +38,6 @@ def cast():
         selection -= c[1]
     print("After a while, you reel in, empty-handed.")
     return 0
-
-score_file_name = "scores.txt"
-
-# Current fisher.
-fisher = None
-# Current fisher's current score.
-score = 0
-# All-time high score.
-high_score = 0
 
 def read_score_file():
     global high_score, high_score_fisher
@@ -50,8 +53,10 @@ def read_score_file():
         write_score_file()
 
 def write_score_file():
+    if fisher == None:
+        return
     score_file = open(score_file_name, "w")
-    score_file.write(str(high_score) + "\n")
+    score_file.write(str(high_score) + " " + fisher + "\n")
     score_file.close()
 
 def one_cast():
@@ -70,10 +75,19 @@ def one_cast():
 
 def game():
     read_score_file()
-    print("High score to date: ", high_score)
+    if high_score_fisher == None:
+        print("High score to date:", high_score)
+    else:
+        print("High score to date:", high_score, "by", high_score_fisher)
+    print()
     while True:
+        global fisher
+        if fisher == None:
+            prompt = "Prepare to fish! "
+        else:
+            prompt = "Fish! "
         try:
-            command = input("Fish! ").split()
+            command = input(prompt).split()
         except EOFError:
             print()
             break
@@ -83,7 +97,6 @@ def game():
             one_cast()
         elif command[0] == "fisher":
             if len(command) == 2:
-                global fisher
                 fisher = command[1]
             else:
                 print("Say \"fisher\" and your fishername. " +
